310c5468a75c360ded88c70c654569457a0b468b
unknown <92805226+Eman-123-eng@users.noreply.github.com>
2022-02-15 01:21:34 +0200

    transfer edit

--- GUI/src/main/java/com/example/gui/transController.java
+++ GUI/src/main/java/com/example/gui/transController.java
@@ -1,4 +1,28 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class transController {
+
+    @FXML
+    private TextField toAccTransfer_field;
+
+    @FXML
+    private TextField transAmount_field;
+
+    @FXML
+    private Label transferLabe;
+
+    @FXML
+    private Button transfer_action;
+
+    @FXML
+    void transfer_action(ActionEvent event) {
+
+    }
+
 }
