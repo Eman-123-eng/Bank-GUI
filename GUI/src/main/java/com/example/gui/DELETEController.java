def3c5c6da080221c29899a45f2944d59c312c2f
unknown <92805226+Eman-123-eng@users.noreply.github.com>
2022-02-16 02:10:37 +0200

    edit and delete design

--- GUI/src/main/java/com/example/gui/DELETEController.java
+++ GUI/src/main/java/com/example/gui/DELETEController.java
@@ -1,4 +1,24 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class DELETEController {
+
+    @FXML
+    private TextField Acc_ID;
+
+    @FXML
+    private TextField Acc_no;
+
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    void deleteBtn(ActionEvent event) {
+
+    }
+
 }
--- GUI/src/main/java/com/example/gui/EDITController.java
+++ GUI/src/main/java/com/example/gui/EDITController.java
@@ -1,4 +1,27 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class EDITController {
+
+    @FXML
+    private TextField Acc_ID;
+
+    @FXML
+    private TextField Acc_no;
+
+    @FXML
+    private TextField Admin_ID;
+
+    @FXML
+    private Button editBtn;
+
+    @FXML
+    void editBtn(ActionEvent event) {
+
+    }
+
 }
