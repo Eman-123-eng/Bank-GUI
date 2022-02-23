33e6b42f031acfd6362ad4533d2b9d83a162be2e
Rowan Omar <61714846+Rowan-Omar@users.noreply.github.com>
2022-02-23 16:07:55 +0200

    .

--- GUI/src/main/java/com/example/gui/Server.java
+++ GUI/src/main/java/com/example/gui/Server.java
@@ -2,6 +2,7 @@ package com.example.gui;
 
 import BankManagement.BankAccount;
 import customer.BankCustomer;
+import javafx.scene.control.Label;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +11,9 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 //Making the PC as a server and Android device as a client
 public class Server extends Thread {
@@ -100,6 +104,7 @@ public class Server extends Thread {
         BufferedReader in;
         PrintWriter out;
         String[] arr;
+        public BankAccount myAcc;
 
         RequestHandler(Socket clientSocket) {
             this.clientSocket = clientSocket;
@@ -107,24 +112,29 @@ public class Server extends Thread {
 
         @Override
         public void run() {
-            try {
-                System.out.println("Received a connection");
+            while(true){
+                try {
+                    System.out.println("Received a connection");
 
-                // Get input and output streams
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    // Get input and output streams
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-                // Firstly, Write out to the client.
+                    // Firstly, Write out to the client.
                 /*outputLine = "Server response";
                 out.println(outputLine);
                 out.flush();*/
 
-                //Sending responses to the client until the client closes the connection
-                inputLine = in.readLine();
-                arr = inputLine.split(",");
-                clientReq = arr[0];
-                if (arr.length > 1)
-                    clientData = arr[1];
+                    //Sending responses to the client until the client closes the connection
+                    inputLine = in.readLine();
+                    if(inputLine == null){
+                        System.out.println("Client does not request");
+                        return;
+                    }
+                    arr = inputLine.split(",");
+                    clientReq = arr[0];
+                    if (arr.length > 1)
+                        clientData = arr[1];
 
                 /*while (inputLine != null && inputLine.length() > 0) {
                     System.out.println("Client: " + inputLine);
@@ -140,27 +150,28 @@ public class Server extends Thread {
                     inputLine = in.readLine();
                 }*/
 
-                System.out.println("Client sends " + clientReq);
+                    System.out.println("Client sends " + clientReq);
 
-                if (clientReq.equals("Exit")) {
-                    System.out.println("Client sends exit");
-                    clientSocket.close();
-                    System.out.println("Connection closed");
-                    return;
+                    if (clientReq.equals("Exit")) {
+                        System.out.println("Client sends exit");
+                        clientSocket.close();
+                        System.out.println("Connection closed");
+                        return;
+                    }
+
+                    handleReq();
+
+                    //out.close();
+
+                    //close the connection
+                    //in.close();
+                    //clientSocket.close();
+
+                    //System.out.println("Connection closed");
+                } catch (IOException e) {
+                    System.out.println("Cannot listen on port " + "7777");
+                    e.printStackTrace();
                 }
-
-                handleReq();
-
-                out.close();
-
-                //close the connection
-                //in.close();
-                //clientSocket.close();
-
-                //System.out.println("Connection closed");
-            } catch (IOException e) {
-                System.out.println("Cannot listen on port " + "7777");
-                e.printStackTrace();
             }
         }
 
@@ -177,6 +188,25 @@ public class Server extends Thread {
             return null;
         }
 
+        public void deposit(double amt){
+            myAcc.setBalance(myAcc.getBalance() + amt);
+            System.out.println(myAcc.getAcctID()+" Id");
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy   HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+
+            if (Objects.equals(myAcc.getOperations().get(0), "---")) myAcc.getOperations().remove(0);
+            myAcc.getOperations().add(myAcc.getOperations().size(), "Deposition:" + amt + "-- at:" + dtf.format(now));
+        }
+        public void withdraw(double amt){
+            myAcc.setBalance(myAcc.getBalance() - amt);
+            System.out.println(myAcc.getAcctID()+" Id");
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy   HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+
+            if (Objects.equals(myAcc.getOperations().get(0), "---")) myAcc.getOperations().remove(0);
+            myAcc.getOperations().add(myAcc.getOperations().size(), "Withdrawal:" + amt + "-- at:" + dtf.format(now));
+        }
+
         public void handleReq() {
             switch (clientReq) {
                 case "signIn": {
@@ -189,15 +219,35 @@ public class Server extends Thread {
                     else if (checked.equals("Admin"))
                         out.println("admin");
                         // out.println("Server: you are an admin");
-                    else if (checked.equals("Customer"))
+                    else if (checked.equals("Customer")) {
                         out.println("customer");
+                        myAcc = BankAccount.getAccount(clientData);
                         //out.println("Server: you are a customer");
-                    else
+                    } else
                         out.println("Incorrectttt");
                     out.flush();
                     break;
                 }
-                case "signUp": {
+                case "deposit": {
+                    if (clientData == null) {
+                        out.println("Enter client amount");
+                        return;
+                    }
+                    deposit(Double.parseDouble(clientData));
+                    BankAccount.writeToFile();
+                    out.println("Server: Deposition,"+ myAcc.getBalance());
+                    out.flush();
+                    break;
+                }
+                case "withdraw": {
+                    if (clientData == null) {
+                        out.println("Enter client amount");
+                        return;
+                    }
+                    withdraw(Double.parseDouble(clientData));
+                    BankAccount.writeToFile();
+                    out.println("Server: Withdrawal,"+ myAcc.getBalance());
+                    out.flush();
                     break;
                 }
                 default: {
